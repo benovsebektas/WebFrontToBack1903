07b1dcff92d01807f0b35bae7419eb55509498ac
benovsebektas <126647571+benovsebektas@users.noreply.github.com>
2023-05-23 00:50:22 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using WebFrontToBack.DAL;
+using WebFrontToBack.ViewModel;
+
+namespace WebFrontToBack.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public HomeController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HttpContext.Session.SetString("name", "Ali");
+            Response.Cookies.Append("surname", "Seferli",new CookieOptions {MaxAge=TimeSpan.FromSeconds(15)});
+            HomeVM homeVM = new HomeVM()
+            {
+                Categories = await _appDbContext.Categories.Where(c => !c.IsDeleted).ToListAsync()
+                //Services = await _appDbContext.Services.Where(c => !c.IsDeleted)
+                //.Include(s=>s.Category)
+                //.Include(s=>s.ServiceImages)
+                //.ToListAsync()
+            };
+            return View(homeVM);
+        }
+
+       
+    }
+}
---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebFrontToBack.DAL;
+using WebFrontToBack.Models;
+
+namespace WebFrontToBack.Controllers
+{
+    public class ServicesController : Controller
+    {
+        protected readonly AppDbContext _context;
+
+        public ServicesController(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.dbServiceCount=await _context.Services.CountAsync();
+
+            return View();
+        }
+
+        public async Task<IActionResult> LoadMore(int skip=0,int take=8)
+        {
+
+            //List<Service> services = await _context.Services
+            //    .OrderByDescending(s => s.Id)
+            //    .Where(s => !s.IsDeleted)
+            //    .Skip(skip)
+            //    .Take(take)
+            //    .Include(s => s.ServiceImages)
+            //    .ToListAsync();
+            //return PartialView("_ServicesPartialView",services);
+
+            return ViewComponent("Service",new {skip=skip,take=take});
+        }
+        public IActionResult GetSession()
+        {
+            string name = HttpContext.Session.GetString("name");
+            string surname = Request.Cookies["surname"];
+            return Json(name+" "+surname);
+        }
+    }
+}
